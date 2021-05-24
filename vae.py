f3d243ca43dc66b54c6f185778eddb27c43b39bf
imgomez0127 <ianm0127@gmail.com>
2021-05-23 20:11:38 -0400

    Finished coding VAE

--- vae.py
+++ vae.py
@@ -13,8 +13,15 @@ class Sampling(Layer):
         return mu + sigma
 
 
-def kl_divergence(mu, sigma_log):
+def kl_loss(mu, sigma_log):
     return -0.5 * tfm.reduce_mean(1 + sigma_log - tfm.square(mu) - tfm.exp(sigma_log), axis=-1)
+
+def compute_conv_shape(image_shape, conv_amt, conv_size = (3,3)):
+    height, width, *rest = image_shape
+    for _ in range(conv_amt):
+        height -= conv_size-1
+        width -= conv_size-1
+    return (height, width, *rest)
 
 class Encoder(Layer):
 
@@ -56,7 +63,24 @@ class Decoder(Layer):
 
 class VAE(Model):
 
-    def __init__(self, image_shape):
+    def __init__(self, image_shape, conv_amt=3):
         super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder(image_shape)
+        self.post_conv_shape = compute_conv_shape(image_shape, conv_amt)
+        self.encoder = Encoder(conv_amt=3)
+        self.decoder = Decoder(image_shape, conv_amt=3)
+
+    def call(self, inputs):
+        encoder_output = self.encoder(inputs)
+        self.add_loss(kl_loss(*encoder_output))
+        return self.decoder(encoder_output)
+
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            reconstruction = self(data, training=True)
+            loss = self.compile_loss(data, reconstruction)
+            loss += self.regularization_weight * sum(self.losses)
+        trainable_vars = self.trainable_vars
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.compiled_metrics.update_state(data, reconstruction)
+        return {m.name: m.result() for m in self.metrics}
